db934c72594b0d2f45c810e0726dc086346038c3
bomsiwor <bomsiwor@gmail.com>
2024-05-17 21:57:26 +0700

    chore (test) : test cicd

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func main() {
 	r.GET("ping", func(ctx *gin.Context) {
 		ctx.JSON(
 			200,
-			map[string]any{"test": true})
+			map[string]any{"test": true, "data": "COba test"})
 	})
 
 	// Initialize all routes
