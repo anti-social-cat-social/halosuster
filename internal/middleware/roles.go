b4b94b7262b597834e505ab6b6e7a0cbc22935b2
bomsiwor <bomsiwor@gmail.com>
2024-05-18 20:41:47 +0700

    feat (authorize middleware) : make the role middleware dynamically settled

--- internal/middleware/roles.go
+++ internal/middleware/roles.go
@@ -10,15 +10,19 @@ import (
 
 // Check if user has a role from the token.
 // Use this on router as middleware
-func HasRoles(r user.UserRole) gin.HandlerFunc {
+func HasRoles(r ...user.UserRole) gin.HandlerFunc {
 	// Check from user HasRoles
 	return func(ctx *gin.Context) {
-		if ctx.GetString("userRole") != string(r) {
-			response.GenerateResponse(ctx, http.StatusUnauthorized, response.WithMessage("User not permitted to do this operation"))
-			ctx.Abort()
-			return
+		role := ctx.GetString("userRole")
+
+		for _, v := range r {
+			if role == string(v) {
+				ctx.Next()
+				return
+			}
 		}
 
-		ctx.Next()
+		response.GenerateResponse(ctx, http.StatusUnauthorized, response.WithMessage("User not permitted to do this operation"))
+		ctx.Abort()
 	}
 }
